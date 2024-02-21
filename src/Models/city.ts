0e249bea1737f50fa06772e1f47abc92c09f9b5e
Guilherme-SilvaH <guihenriquesilva10@gmail.com>
2024-02-21 20:05:33 -0300

    get-date

--- src/Models/city.ts
+++
@@ -1,5 +0,0 @@
-import { ObjectId } from "mongodb"
-
-export default class city {
-    constructor(public name: string, public temp: number, public id?: ObjectId) {}
-}
--- src/apiServer.ts
+++ src/apiServer.ts
@@ -25,6 +25,7 @@ app.use(express.json())
 const client = new MongoClient(keyConexao!)
 
 
+// get para pegar inf da api Weather
 app.get('/', async(req, res) => {
     const {city} = req.query
     try{
@@ -42,7 +43,7 @@ app.get('/', async(req, res) => {
 
 
         //Objeto com os dados da resposta modificados
-        const resMod = {
+        const resObj = {
             cidade: city || weatherData.name,
             Location: locationName,
             Region: locationRegion,
@@ -59,7 +60,7 @@ app.get('/', async(req, res) => {
         const collection = db.collection(process.env.CITYS!);
 
         //Inserção dos dados modificados no banco de dados
-        const result = await collection.insertOne(resMod);
+        const result = await collection.insertOne(resObj);
         console.log(`Insert city in the database, ID: ${result.insertedId}`);
 
         
@@ -73,6 +74,51 @@ app.get('/', async(req, res) => {
 
 
 })
+
+
+//app para filtrar as inf
+app.get('/date', async(req, res) => {
+    const {startDate, endDate} = req.query
+
+    try{
+       
+    // Conexão com o banco de dados MongoDB
+        await client.connect();
+        const db = client.db(process.env.DB_NAME);
+        const collection = db.collection(process.env.CITYS!);
+
+        // Função para converter a data para o formato adequado no MongoDB
+        function filter(date:string) {
+            return new Date(date).toISOString();       
+        };
+
+
+        //dateFilter é construído com uma condição para o campo timestamp no formato de uma consulta para MongoDB
+        const dateFilter = startDate && endDate ? {
+            timestamp: {
+                // é maior ou igual ($gte - greater than or equal) ao resultado de formatToISO(startDate.toString())
+                $gte: filter(startDate.toString()),
+
+                //é menor ou igual ($lte - less than or equal) ao resultado de formatToISO(endDate.toString()).
+                $lte: filter(endDate.toString())
+            }
+        }
+        //Se startDate ou endDate não estiverem definidos (ou seja, algum deles é false), o objeto dateFilter é definido como um objeto vazio {}.
+        : {};
+
+        // Consulta no banco de dados utilizando o filtro de datas
+        const resultDate = await collection.find(dateFilter).toArray();
+        console.log(`Result filtered from the date in the database: ${resultDate}`);
+        res.json(resultDate);
+        
+    }catch(error){
+        // Tratamento de erros
+        console.error("Error fetching dates");
+        res.status(500).send("Internal Server Error");
+    }finally{
+        await client.close();
+    }
+});
 
 
 
