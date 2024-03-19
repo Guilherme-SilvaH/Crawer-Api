ec48d8bceea742120568afb4d9998c1313dcdf7a
Guilherme-SilvaH <guihenriquesilva10@gmail.com>
2024-03-19 16:26:39 -0300

    lambda.js

---
+++ dist/lambda.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+
+exports.handler = async (event) => {
+    try {
+        const defaultCity = "Paulinia"; 
+        
+        // Chamar a API de atualização de previsões usando o método POST
+        const response = await axios.post('http://54.89.145.146:8080/weather?city=Paulinia', {
+            city: defaultCity
+        });
+
+        // Verificar a resposta da API e retornar os resultados
+        if (response.status === 200) {
+            return {
+                statusCode: 200,
+                body: JSON.stringify(response.data)
+            };
+        } else {
+            return {
+                statusCode: response.status,
+                body: JSON.stringify({ message: 'Erro ao chamar a API de atualização de previsões' })
+            };
+        }
+    } catch (error) {
+        console.error("Erro ao chamar a API de atualização de previsões:", error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: 'Erro interno do servidor' })
+        };
+    }
+};
